9376f0826360a54bf3068e525ec387207032b700
DEV H GOWDA <113254147+DevHGowda@users.noreply.github.com>
2023-04-14 12:02:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 
 const homeStartingContent = "Introducing a dynamic and engaging blog website that provides readers with high-quality content on a variety of topics. Our blog website is designed to be user-friendly and easy to navigate, with a clean and modern layout that appeals to a wide range of readers";
 const aboutContent = "Introducing a dynamic and engaging blog website that provides readers with high-quality content on a variety of topics. Our blog website is designed to be user-friendly and easy to navigate, with a clean and modern layout that appeals to a wide range of readers";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const contactContent = "Introducing a dynamic and engaging blog website that provides readers with high-quality content on a variety of topics. Our blog website is designed to be user-friendly and easy to navigate, with a clean and modern layout that appeals to a wide range of readers";
 
 const app = express();
 
